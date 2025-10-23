bfd5e60d6bf1c21cf1897d2bf739f4d82dfb925b
Victor Hugo Cantu Ramires <victorxtz07@gmail.com>
2025-10-22 21:54:09 -0300

    Feito as configurações do tailwind e shadoncn

--- src/cases/categories/services/category.service.ts
+++ src/cases/categories/services/category.service.ts
@@ -11,14 +11,22 @@ export const CategoryService = {
     },
 
     async create(category: CategoryDTO): Promise<CategoryDTO> {
-        const result = await api.post(_ENDPOINT);
+        const result = await api.post(_ENDPOINT, category);
         return result.data;
     },
 
-    async getById(id: string): Promise<CategoryDTO> {},
+    async getById(id: string): Promise<CategoryDTO> {
+        const result = await api.get(`${_ENDPOINT}/${id}`);
+        return result.data;
+    },
 
-    async update(id: string, category: CategoryDTO): Promise<CategoryDTO> {},
+    async update(id: string, category: CategoryDTO): Promise<CategoryDTO> {
+        const result = await api.put(`${_ENDPOINT}/${id}`, category);
+        return result.data;
+    },
 
-    async delete(id: string): Promise<void>{}
+    async delete(id: string): Promise<void>{
+        await api.delete(`${_ENDPOINT}/${id}`)
+    }
 
 }
